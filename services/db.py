454fdf6e6e960dc145a80b2354b82482cdce9536
Gabor Feher <a.gabor.feher@gmail.com>
2022-04-22 11:30:48 +0100

    bit of code cleaning

--- services/db.py
+++ services/db.py
@@ -1,12 +1,14 @@
 from neo4j import GraphDatabase, basic_auth
 
+# Note: for production, credentials should be set via environment variable
+NEO4J_IP = "bolt://44.202.164.164:7687"
+NEO4J_PASS = "steamer-panels-words"
+
 
 class Neo4jConnection:
     def __init__(self):
         try:
-            # Note: for production, credentials should be set via environment variable
-            self._driver = GraphDatabase.driver("bolt://44.202.164.164:7687",
-                                               auth=basic_auth("neo4j", "steamer-panels-words"))
+            self._driver = GraphDatabase.driver(NEO4J_IP, auth=basic_auth("neo4j", NEO4J_PASS))
         except Exception as e:
             print('Driver creation error:', e)
 
